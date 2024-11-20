e2396408e339f0cc5cb163faa85c94c93c677d41
MathisGau <126653891+MathisGau@users.noreply.github.com>
2024-11-20 12:14:55 +0100

    Register + Style

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,12 +3,22 @@
 import { useRouter } from "next/navigation";
 import { useCallback } from "react";
 import Button from "./components/Button";
+import Card from "./components/Card";
 
 export default function Home() {
   const router = useRouter();
-  const handleClick = useCallback(() => {
+  const handleClickLogIn = useCallback(() => {
     router.push("/login");
   }, []);
+  const handleClickRegister = useCallback(() => {
+    router.push("/register");
+  }, []);
 
-  return <Button onClick={handleClick}>Log in</Button>;
+  return (
+    <Card title="Card">
+      <h1>Home</h1>
+      <Button onClick={handleClickLogIn}>Log in</Button>
+      <Button onClick={handleClickRegister}>Register</Button>
+    </Card>
+  );
 }
---
+++ src/app/register/page.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Button from "../components/Button";
+import Card from "../components/Card";
+import Text from "../components/Text";
+
+export default function Login() {
+  return (
+    <>
+      <Card title="Card">
+        <div className=" flex flex-col">
+          <Button>Register</Button>
+        </div>
+      </Card>
+    </>
+  );
+}
