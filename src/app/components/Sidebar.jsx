ea69d02d23ab682cb753daf3797f55f836fa938c
MathisGau <126653891+MathisGau@users.noreply.github.com>
2025-01-09 16:36:16 +0100

    Ajout du bouton de deconnection et d'un attribut pour surcharger le CSS du component Button

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -4,18 +4,25 @@ import { useRouter } from "next/navigation";
 import { useCallback } from "react";
 import SidebarButton from "./SidebarButton";
 import Separator from "./Séparator";
+import Button from "./Button";
 
 export default function Sidebar() {
   const router = useRouter();
+
   const handleClickObjectives = useCallback(() => {
     router.push("/dashboard/objectives");
-  }, []);
+  }, [router]);
+
   const handleClickSkills = useCallback(() => {
     router.push("/dashboard/skills");
-  }, []);
+  }, [router]);
+
+  const handleLogout = useCallback(() => {
+    router.push("/login");
+  }, [router]);
 
   return (
-    <div className="h-screen w-64 bg-green-800 p-10 flex flex-col gap-4">
+    <div className="h-screen w-64 bg-green-800 p-10 flex flex-col gap-4 relative">
       <h1 className="w-full py-2 text-center text-white font-bold text-lg">
         IDENTIFIANT
       </h1>
@@ -23,6 +30,10 @@ export default function Sidebar() {
 
       <SidebarButton onClick={handleClickObjectives}>Objectives</SidebarButton>
       <SidebarButton onClick={handleClickSkills}>Skills</SidebarButton>
+
+      <Button onClick={handleLogout} className="mt-auto self-center">
+        Déconnexion
+      </Button>
     </div>
   );
 }
